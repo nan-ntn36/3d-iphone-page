256a66627c81abf207fb932ea06cd0fcc5673646
TrungNghia <it29@katsuravn.com>
2026-01-15 17:05:34 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline flex justify-center items-center h-screen">
+      TEST
+    </div>
+  );
+};
+
+export default App;
