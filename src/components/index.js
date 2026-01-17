fb5b1ac3db9dd195c2d81375c46cea90506e170c
TrungNghia <it29@katsuravn.com>
2026-01-17 14:40:00 +0700

    feat: add hightlight slide video

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Hero from './Hero';
 import Navbar from './Navbar';
 import HightLights from './HightLights';
+import VideoCarousel from './VideoCarousel';
 
-export { Hero, Navbar, HightLights };
+export { Hero, Navbar, HightLights, VideoCarousel };
